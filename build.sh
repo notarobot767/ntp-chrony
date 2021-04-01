bf7ff7d7081158692fe1ffdb8b24ba539d5718e9
Noah Ogrydziak <12644755+notarobot767@users.noreply.github.com>
2021-04-01 13:59:51 -0400

    added pull option

--- build.sh
+++ build.sh
@@ -4,7 +4,13 @@ cd "${0%/*}"
 #change directory relative to script
 
 source ./config/config.conf
-
-podman build -f "./config/dockerfile.conf" \
+docker build \
+	--file "./config/dockerfile.conf" \
 	--tag $IMAGE_NAME \
+	--pull \
 	.
+
+#--file: specify dockerfile since in subdirectory
+#--tag: name of the image
+#--pull always pull latest base image
+#https://docs.docker.com/engine/reference/commandline/build/
